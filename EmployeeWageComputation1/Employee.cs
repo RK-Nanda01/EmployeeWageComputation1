b69a689cfc18d1d8157fa1969483124cc2d8cafa
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-07-21 09:51:34 +0530

    [RK Nanda] Adding method to calculate wage for multiple comp

--- EmployeeWageComputation1/Employee.cs
+++ EmployeeWageComputation1/Employee.cs
@@ -6,20 +6,32 @@ namespace EmployeeWageComputation1
         // Use of private variables //
 		private const int FULL_TIME = 1;
         private const int PART_TIME = 2;
-        private const int RATE_PER_HOUR = 20;
-		private const int FULL_DAY_HOUR = 8;
-        private const int PART_TIME_HOUR = 4;
-        private const int TOTAL_WORKING_DAY = 20;
-        private const int TOTAL_WORKING_HOURS = 100;
+        private  int ratePerHour;
+        private int fullDayHour = 8;
+        private  int partTimeHour= 4;
+        private  int totalWorkingDays;
+        private  int workingHoursPerMonth;
+
+        private string company;
         private int empHours = 0;
         //Use of method inside a class//
+
+        public Employee(string cmp, int ratePerHour, int totalWorkingDays, int workingHoursPerMonth)
+        {
+            this.company = cmp;
+            this.ratePerHour = ratePerHour;
+            this.totalWorkingDays = totalWorkingDays;
+            this.workingHoursPerMonth = workingHoursPerMonth;
+        }
+
+
         public void MonthlyWage()
         {
             int totalHours = 0;
             int totalWage=0;
             int day=1;
 
-            while (totalHours <= TOTAL_WORKING_HOURS && day <= TOTAL_WORKING_DAY)
+            while (totalHours <= this.workingHoursPerMonth && day <= this.totalWorkingDays)
             {
                 Random rnd = new Random();
                 int rnd_num = rnd.Next(0, 3);
@@ -29,13 +41,13 @@ namespace EmployeeWageComputation1
 
                     case PART_TIME:
                         {
-                            empHours = PART_TIME_HOUR;
+                            empHours = this.partTimeHour;
                             break;
                         }
 
                     case FULL_TIME:
                         {
-                            empHours = FULL_DAY_HOUR;
+                            empHours = this.fullDayHour;
                             break;
                         }
 
@@ -52,7 +64,7 @@ namespace EmployeeWageComputation1
                 day++;
             }
 
-            totalWage = totalHours * RATE_PER_HOUR;
+            totalWage = totalHours * this.ratePerHour;
 
             Console.WriteLine($"Total Wage for {day - 1} is {totalWage}");
         }
--- EmployeeWageComputation1/Program.cs
+++ EmployeeWageComputation1/Program.cs
@@ -4,8 +4,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Employee Wage Computation!");
-        Employee e = new Employee();
+        Employee e = new Employee("Deloitte", 20, 20, 100);
+        Employee f = new Employee("Google", 30, 20, 100);
         e.MonthlyWage();
+        f.MonthlyWage();
         Console.ReadLine();
 
     }
