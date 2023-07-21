3d4c103fd353c9b19d4e6438ba403fc68e0c0f6c
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-07-21 08:39:09 +0530

    [RK Nanda] Adding method to check if employee present

---
+++ EmployeeWageComputation1/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace EmployeeWageComputation1
+{
+	public class Employee
+	{
+		public const int IS_PRESENT = 1;
+
+		public void IsPresent()
+		{
+			Random rnd = new Random();
+			int num = rnd.Next(0, 2);
+
+			if(num == IS_PRESENT)
+			{
+				Console.WriteLine("The Employee Is Present");
+			}
+			else
+			{
+				Console.WriteLine("The Employee is Absent");
+			}
+
+		}
+	}
+}
+
--- EmployeeWageComputation1/Program.cs
+++ EmployeeWageComputation1/Program.cs
@@ -4,7 +4,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Employee Wage Computation!");
+        Employee e = new Employee();
+        e.IsPresent();
         Console.ReadLine();
+
     }
 }
 
