589810cdff436bb16963456022bf10b945b35995
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-07-21 08:35:18 +0530

    [RK Nanda] Adding a Welcome Message

---
+++ EmployeeWageComputation1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageComputation1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Employee Wage Computation!");
+        Console.ReadLine();
+    }
+}
+
